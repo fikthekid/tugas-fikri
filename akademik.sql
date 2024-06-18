b9f5cc3a33548dc9f3051e21283578cafeda1878
fikthekid <2206127@itg.ac.id>
2024-06-18 19:38:24 +0700

    first commit

---
+++ akademik.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 18, 2024 at 02:33 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akademik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `absensi`
+--
+-- Error reading structure for table akademik.absensi: #1932 - Table &#039;akademik.absensi&#039; doesn&#039;t exist in engine
+-- Error reading data for table akademik.absensi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `akademik`.`absensi`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dosen`
+--
+-- Error reading structure for table akademik.dosen: #1932 - Table &#039;akademik.dosen&#039; doesn&#039;t exist in engine
+-- Error reading data for table akademik.dosen: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `akademik`.`dosen`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+-- Error reading structure for table akademik.kelas: #1932 - Table &#039;akademik.kelas&#039; doesn&#039;t exist in engine
+-- Error reading data for table akademik.kelas: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `akademik`.`kelas`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+-- Error reading structure for table akademik.mahasiswa: #1932 - Table &#039;akademik.mahasiswa&#039; doesn&#039;t exist in engine
+-- Error reading data for table akademik.mahasiswa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `akademik`.`mahasiswa`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matkul`
+--
+-- Error reading structure for table akademik.matkul: #1932 - Table &#039;akademik.matkul&#039; doesn&#039;t exist in engine
+-- Error reading data for table akademik.matkul: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `akademik`.`matkul`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
